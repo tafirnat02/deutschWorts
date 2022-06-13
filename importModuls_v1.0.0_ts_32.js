0d0d214d7e6d73a3314fa49c7b03dd2cb16a2cb8
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-06-13 21:50:31 +0200

    fdfd

--- importModuls_v1.0.0_ts_32.js
+++ importModuls_v1.0.0_ts_32.js
@@ -41,7 +41,7 @@ const reorganizer = clear =>{
       return abfrage.neu = exList ;
     }else if(localWortObj !== null ){
       let localWortArr = [],shortWortList,allLocalList;
-      for(key in localWortObj){localWortArr.push(key)}
+      for(let k_ in localWortObj)localWortArr.push(k_)
       allLocalList=localWortArr.join(",")
       shortWortList=localWortArr.slice(0,10).join(',') + (localWortArr.length>10?'...':'')
       let localWort = confirm(`🪃 Sayfada yakalanan kelimeler bulunmakta.\n🧭 Bu kelime listesi icin islem yapilsin mi?\📌Kelimeler:${shortWortList}`)
